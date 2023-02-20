fa8b59bd4960b88180ebe57c1ae1a100b40b2793
a-far <82075798+a-far@users.noreply.github.com>
2023-02-20 16:41:06 +0800

    Add files via upload

---
+++ opus/宣传类/js/javascript.js
@@ -0,0 +1,19 @@
+const pictures = document.querySelectorAll('.picture')
+
+pictures.forEach(picture => {			//遍历picture
+    picture.addEventListener('click', () => {		//点击picture触发事件
+        removeActiveClasses()
+        picture.classList.add('active')			//类(class) 由picture => picture active
+			
+						
+    })
+})
+
+
+function removeActiveClasses() {
+    pictures.forEach(picture => {
+        picture.classList.remove('active')
+    })
+}
+
+
